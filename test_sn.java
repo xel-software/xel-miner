a07efb82f6eac97c28ee10837fdf048fa0fa985d
Sprocket <cryptoflyr@gmail.com>
2017-05-31 21:49:21 -0400

    Add files via upload

---
+++ test_sn.java
@@ -0,0 +1,169 @@
+import java.net.*;
+import java.io.*;
+
+class API
+{
+	static private final int MAXRECEIVESIZE = 65535;
+
+	static private Socket socket = null;
+
+	private void closeAll() throws Exception
+	{
+		if (socket != null)
+		{
+			socket.close();
+			socket = null;
+		}
+	}
+
+	public void display(String result) throws Exception
+	{
+		String value;
+		String name;
+		String[] sections = result.split("\\|", 0);
+
+		for (int i = 0; i < sections.length; i++)
+		{
+			if (sections[i].trim().length() > 0)
+			{
+				String[] data = sections[i].split(",", 0);
+
+				for (int j = 0; j < data.length; j++)
+				{
+					String[] nameval = data[j].split("=", 2);
+
+					if (j == 0)
+					{
+						if (nameval.length > 1
+						&&  Character.isDigit(nameval[1].charAt(0)))
+							name = nameval[0] + nameval[1];
+						else
+							name = nameval[0];
+
+						System.out.println("[" + name + "] =>");
+						System.out.println("(");
+					}
+
+					if (nameval.length > 1)
+					{
+						name = nameval[0];
+						value = nameval[1];
+					}
+					else
+					{
+						name = "" + j;
+						value = nameval[0];
+					}
+
+					System.out.println("   ["+name+"] => "+value);
+				}
+				System.out.println(")");
+			}
+		}
+	}
+
+	public void process(String cmd, InetAddress ip, int port) throws Exception
+	{
+		StringBuffer sb = new StringBuffer();
+		char buf[] = new char[MAXRECEIVESIZE];
+		int len = 0;
+
+System.out.println("Attempting to send '"+cmd+"' to "+ip.getHostAddress()+":"+port);
+
+		try
+		{
+//			socket = new Socket(ip, port);
+			PrintStream ps = new PrintStream(socket.getOutputStream());
+			ps.print(cmd.toCharArray());
+			ps.flush();
+
+			InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+			while (1)
+			{
+				len = isr.read(buf, 0, MAXRECEIVESIZE);
+				if (len < 1)
+					break;
+				sb.append(buf, 0, len);
+				if (buf[len-1] == '\0')
+					break;
+			}
+
+//			closeAll();
+		}
+		catch (IOException ioe)
+		{
+			System.err.println(ioe.toString());
+			closeAll();
+			return;
+		}
+
+		String result = sb.toString();
+
+		System.out.println("Answer='"+result+"'");
+
+		display(result);
+	}
+
+	public API(String command, String _ip, String _port) throws Exception
+	{
+		InetAddress ip;
+		int port;
+
+		try
+		{
+			ip = InetAddress.getByName(_ip);
+		}
+		catch (UnknownHostException uhe)
+		{
+			System.err.println("Unknown host " + _ip + ": " + uhe);
+			return;
+		}
+
+		try
+		{
+			port = Integer.parseInt(_port);
+		}
+		catch (NumberFormatException nfe)
+		{
+			System.err.println("Invalid port " + _port + ": " + nfe);
+			return;
+		}
+
+		process(command, ip, port);
+	}
+
+	public static void main(String[] params) throws Exception
+	{
+		String ip = "127.0.0.1";
+		String port = "4016";
+
+		// Open Socket To SuperNode
+		socket = new Socket( InetAddress.getByName(ip), Integer.parseInt(port) );
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char key;
+		System.out.println("1) Get Status");
+		System.out.println("2) Validate Job");
+		System.out.println("3) Validate POW");
+		System.out.println("4) Validate Bounty");
+		System.out.println("Enter 'q' to quit.");
+		do {
+			key = (char) br.read();
+
+			if (key == '1') {
+				command = "{\"req_id\": 123,\"req_type\": 1,\"source\": \"\",\"input\": \"\",\"state\": \"\"}";
+				new API(command, ip, port);
+			}
+			else if (key == '2') {
+				command = "{\"req_id\": 456,\"req_type\": 1,\"source\": \"@<-BsH!b9'F<D\\K0eb<h@<-BsH!b].DKI!D0eb@E$=Rsq@<l3rDf021+>GQ+3soD:Eaa6#F_ku6B-8o_1cl%QEcPT6?Y4+m@<<VH0Jtp!@<-BsH!b*#F^f/u+>GQ.3sl=,F`(]2Bl@l3D..-r+F=G%Bj3;t+?^i%3sl::>;BJ,4WlLA$41NQ1L2+d+>Z(d$$C&g1gM4e+>c.e$\"dC!>p)9Q2(gUF$416I2I.Fg+>ti-3spBC$>+Eu@ruF'DBO+6EbT-2+F=G<+ED%7F_l.B-o*4YI/\",\"input\": \"\",\"state\": \"\"}";
+				new API(command, ip, port);
+			}
+			else if (key == '3') {
+				command = "{\"req_id\": 789,\"req_type\": 1,\"source\": \"\",\"input\": \"abc\",\"state\": \"def\"}";
+				new API(command, ip, port);
+			}
+
+		} while(key != 'q');
+	
+	}
+}
---
+++ xel_supernode.c
@@ -0,0 +1,185 @@
+/*
+* Copyright 2017 sprocket
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the Free
+* Software Foundation; either version 2 of the License, or (at your option)
+* any later version.
+*/
+
+#define SUPERNODE_VERSION "0.1"
+
+#ifdef WIN32
+#define  _WINSOCK_DEPRECATED_NO_WARNINGS
+#endif
+
+#include "miner.h"
+
+#ifndef WIN32
+# include <errno.h>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include <netdb.h>
+#else
+#include <ws2tcpip.h>
+# include <winsock2.h>
+#endif
+
+#ifndef INVALID_SOCKET
+#define INVALID_SOCKET -1
+#endif
+
+#ifndef INADDR_NONE
+#define INADDR_NONE -1
+#endif
+
+#ifndef in_addr_t
+#define in_addr_t uint32_t
+#endif
+
+#define MAX_QUEUED_CONNECTIONS 1
+#define SOCKET_BUF_SIZE 100000	// Need To Fix This
+
+static void sn_get_request()
+{
+	char ip[] = "127.0.0.1";	// For Now All Connections Are On LocalHost
+	unsigned short port = 4016;
+	char buf[SOCKET_BUF_SIZE];
+	int i, n, len;
+	struct sockaddr_in server = { 0 };
+	struct sockaddr_in client = { 0 };
+	json_t *val;
+	json_error_t err;
+	uint32_t req_id, req_type;
+
+#ifdef WIN32
+	SOCKET s, core;
+#else
+	long s, core;
+#endif
+
+
+	sleep(1);
+
+	// Create Socket For Core Server To Connect To
+	s = socket(AF_INET, SOCK_STREAM, 0);
+	if (s == INVALID_SOCKET) {
+		applog(LOG_ERR, "ERROR: Unable to create SuperNode socket (%s)", strerror(errno));
+		goto out;
+	}
+
+	// Prepare Socket Address For SuperNode
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	server.sin_addr.s_addr = inet_addr(ip);
+	server.sin_port = htons(port);
+	if (server.sin_addr.s_addr == (in_addr_t)INADDR_NONE) {
+		applog(LOG_ERR, "ERROR: Unable to configure SuperNode IP address (%s)", strerror(errno));
+		goto out;
+	}
+
+	// Bind Socket To SuperNode Port (6 Tries Max)
+	applog(LOG_DEBUG, "DEBUG: Binding Socket to port %d", port);
+	for (i = 0; i < 10; i++) {
+		n = bind(s, (struct sockaddr *)(&server), sizeof(server));
+		if (n >= 0)
+			break;
+		sleep(10);
+	}
+
+	if (n < 0) {
+		applog(LOG_ERR, "ERROR: Unable to bind Socket to port $d (%s)", port, strerror(errno));
+		goto out;
+	}
+
+	// Listen For Incoming Connections
+	if (listen(s, MAX_QUEUED_CONNECTIONS) < 0 ) {
+		applog(LOG_ERR, "ERROR: Unable to set Queue for SuperNode socket (%s)", strerror(errno));
+		goto out;
+	}
+
+
+// TODO:  Add loop to reconnect
+
+	// Accept Connection From Core Server
+	len = sizeof(struct sockaddr_in);
+	core = accept(s, (struct sockaddr *)&client, &len);
+	if (core < 0) {
+		applog(LOG_ERR, "ERROR: Unable to accept connection to SuperNode (%s)", strerror(errno));
+		goto out;
+	}
+
+	if (client.sin_addr.S_un.S_addr != server.sin_addr.S_un.S_addr)
+		applog(LOG_NOTICE, "SuperNode connected to Elastic Core Server");
+	else {
+		applog(LOG_ERR, "ERROR: SuperNode blocked connection from IP: %s", inet_ntoa(client.sin_addr));
+//		continue;
+	}
+
+	while (1) {
+
+		// Read Request Into Buffer
+		n = recv(core, &buf[0], SOCKET_BUF_SIZE, 0);
+		buf[n] = '\0';
+
+		// Get Request ID / Type From JSON
+		val = JSON_LOADS(buf, &err);
+		if (!val) {
+			applog(LOG_ERR, "ERROR: JSON decode failed (code=%d): %s", err.line, err.text);
+
+			// Send Acknowledgment To Core Server (Error)
+			n = send(core, "ERROR", 6, 0);
+		}
+		else {
+			// Send Acknowledgment To Core Server (OK)
+			n = send(core, "OK", 3, 0);
+
+			//		if (opt_protocol) {
+				char *str = json_dumps(val, JSON_INDENT(3));
+				applog(LOG_DEBUG, "DEBUG: JSON SuperNode Request -\n%s", str);
+				free(str);
+			//		}
+
+			req_id = (uint32_t)json_integer_value(json_object_get(val, "req_id"));
+			req_type = (uint32_t)json_integer_value(json_object_get(val, "req_type"));
+
+			json_decref(val);
+
+			applog(LOG_DEBUG, "DEBUG: Req_Id: %d, Req_Type: %d Received", req_id, req_type);
+		}
+
+		// Reset If There Is A Connection Error
+		if (n <= 0)
+			break;
+
+
+// TODO Create Queues For Each Req Type
+
+
+	}
+
+out:
+
+#ifdef WIN32
+	if (s != INVALID_SOCKET)
+		closesocket(s);
+#else
+	if (s)
+		close(s);
+#endif
+
+	return;
+}
+
+
+extern void *supernode_thread(void *userdata)
+{
+	struct thr_info *mythr = (struct thr_info*)userdata;
+
+	applog(LOG_NOTICE, "Starting SuperNode Thread...");
+	sn_get_request();
+	tq_freeze(mythr->q);
+
+	return NULL;
+}
