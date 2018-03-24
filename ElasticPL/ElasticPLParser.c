eabf84df88ac1d064bfdc720ebd7902fe191b945
Starfish <noreply@github.com>
2018-03-25 09:46:31 +1000

    errno fixes

--- ElasticPL/ElasticPLParser.c
+++ ElasticPL/ElasticPLParser.c
@@ -754,7 +754,7 @@ static bool create_exp(SOURCE_TOKEN *token, int token_num) {
 					// Convert Hex Numbers
 					if ((len > 2) && (token->literal[0] == '0') && (token->literal[1] == 'x')) {
 						if (len > 18) {
-							applog(LOG_ERR, "Syntax Error: Line: %d - Hex value exceeds 64 bits (%s, len %d, errno %d)", token->line_num, token->literal, len, errno);
+							applog(LOG_ERR, "Syntax Error: Line: %d - Hex value exceeds 64 bits (%s, len %d)", token->line_num, token->literal, len);
 							return false;
 						}
 						else if (len < 11) {
@@ -770,7 +770,7 @@ static bool create_exp(SOURCE_TOKEN *token, int token_num) {
 					// Convert Binary Numbers
 					else if ((len > 2) && (token->literal[0] == '0') && (token->literal[1] == 'b')) {
 						if (len > 66) {
-							applog(LOG_ERR, "Syntax Error: Line: %d - Binary value exceeds 64 bits (%s, len %d, errno %d)", token->line_num, token->literal, len, errno);
+							applog(LOG_ERR, "Syntax Error: Line: %d - Binary value exceeds 64 bits (%s, len %d)", token->line_num, token->literal, len);
 							return false;
 						}
 						else if (len < 35) {
@@ -785,6 +785,7 @@ static bool create_exp(SOURCE_TOKEN *token, int token_num) {
 
 					// Convert Integer Numbers
 					else if (len > 0) {
+						errno = 0;
 						if (is_signed)
 							val_int64 = strtoll(&token->literal[0], NULL, 10);
 						else
@@ -815,6 +816,7 @@ static bool create_exp(SOURCE_TOKEN *token, int token_num) {
 				}
 				else if (token->data_type == DT_FLOAT) {
 
+					errno = 0;
 					val_double = strtod(&token->literal[0], NULL);
 
 					if (errno) {
